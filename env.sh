e6b4b039a942e865ea00099da3014faed47bf301
Cristian <>
2017-12-05 14:54:54 +0000

    2paddles trained models

---
+++ env.sh
@@ -0,0 +1,2 @@
+PROJ_ROOT=/Users/cristian/dev/cs229-proj
+export PYTHONPATH=$PROJ_ROOT/rl-teacher/rl-teacher/:$PROJ_ROOT/rl-teacher/rl-teacher/agents/parallel-trpo/:$PROJ_ROOT/rl-teacher/rl-teacher/agents/pposgd-mpi/:$PROJ_ROOT/rl-teacher/rl-teacher/agents/simple-trpo/:$PROJ_ROOT/rl-teacher/rl-teacher/human-feedback-api:$PROJ_ROOT/unity/unityagents/:$PROJ_ROOT/unity/
